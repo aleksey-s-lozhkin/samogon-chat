b75aab70c7ebcc50b63c3cb47e8508bad10b11dd
Ivan Glushenkov <glushman966@gmail.com>
2026-08-27 20:36:33 +0300

    добавлена обработка статики в urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,19 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
+from config import settings
+from config.views import home
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("chat/", include("chat.urls")),
+    path("users/", include("users.urls")),
+    path("", home, name="home"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
